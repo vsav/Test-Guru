f1bbdcc9fcd54b9362d02e436aed4067a0a7c1ac
Vadim Savostin <vsavdev@gmail.com>
2019-09-04 17:39:29 +0300

    remove symbol from tests.where

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   def show_tests(level)
     tests = Test.where(id: JoinUserTest.where(user_id: id).pluck(:test_id))
-    tests.where('level = :level', level: level).pluck(:title)
+    tests.where('level = ?', level).pluck(:title)
   end
 end
 
