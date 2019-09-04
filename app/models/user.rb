bd4790b9eeaa3a5ebef1de26b66a7534a0e07f15
Vadim Savostin <vsavdev@gmail.com>
2019-09-04 21:53:16 +0300

    fix show_tests method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   def show_tests(level)
-    tests = Test.where(id: JoinUserTest.where(user_id: id).pluck(:test_id))
-    tests.where('level = ?', level)
+    UserTest.joins(:user, :test).where(users: { id: id }, tests: { level: level })
   end
 end
-
