b0b88495fb0a8af0ff3977adfd357d4c6e6829bd
Vadim Savostin <vsavdev@gmail.com>
2019-09-05 17:19:11 +0300

    use sql join insteed of associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   def show_tests(level)
-    UserTest.joins(:user, :test).where(users: { id: id }, tests: { level: level })
+    Test.joins('JOIN user_tests ON tests.id = user_tests.test_id')
+        .where('user_tests.user_id = ? AND level = ?', id, level)
   end
 end
