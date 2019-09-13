eb943ec68455c73b4d9974f361afe1b35a035437
Vadim Savostin <vsavdev@gmail.com>
2019-09-14 00:59:10 +0300

    add question_header helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,8 @@
 module QuestionsHelper
+
+  def question_header(question)
+    question.new_record? ? "Create new #{question.test.title} question" : "Edit #{question.test.title} question"
+  end
+
+
 end
