3922d8fb45783bcae0d873d08f40a26a226719a6
Vadim Savostin <vsavdev@gmail.com>
2019-09-17 19:11:52 +0300

    errors fixed

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,16 +3,12 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_save :before_save_set_next_question
 
   SUCCESS = 85.0
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
-
-    self.current_question = next_question
+    self.correct_questions += 1 if correct_answer?(answer_ids)
     save!
   end
 
@@ -28,7 +24,7 @@ class TestPassage < ApplicationRecord
     test_result >= SUCCESS
   end
 
-  def current_question_id
+  def current_question_number
     test.questions.order(:id).pluck(:id).index(current_question.id) + 1
   end
 
@@ -38,15 +34,12 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    if current_question.nil?
-      self.current_question = test.questions.first if test.present?
-    else
-      self.current_question = next_question
-    end
+  def before_save_set_next_question
+    self.current_question = next_question
   end
 
   def correct_answer?(answer_ids)
+    answer_ids ||= []
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
@@ -55,7 +48,11 @@ class TestPassage < ApplicationRecord
   end
 
   def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+    if current_question.nil?
+      self.current_question = test.questions.first
+    else
+      test.questions.order(:id).where('id > ?', current_question.id).first
+    end
   end
 
 end
