a67149c95be22638e77eacf9c1c60fea5cfa19c1
Vadim Savostin <vsavdev@gmail.com>
2019-09-17 20:44:25 +0300

    helper refactoring

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,5 +1,11 @@
 module TestPassagesHelper
   def test_summary(test)
-    test.test_passed? ? 'passed' : 'failed'
+    if test.test_passed?
+      @message = "You have passed the test"
+      @result = 'passed'
+    else
+      @message = "You have failed the test"
+      @result = 'failed'
+    end
   end
 end
