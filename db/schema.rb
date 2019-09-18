51215e1812f23f93892c3bac359cf39578bc6aa9
Vadim Savostin <vsavdev@gmail.com>
2019-09-18 05:18:24 +0300

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_154839) do
+ActiveRecord::Schema.define(version: 2019_09_17_193144) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_09_16_154839) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
