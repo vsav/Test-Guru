d0267e01b82b956222dea42d1910b27325d0d38a
Vadim Savostin <vsavdev@gmail.com>
2019-09-18 05:13:25 +0300

    user model refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+
+  EMAIL_FORMAT = /\A[^@\s]+@[^@\s]+\z/.freeze
+
+  #include Auth
+
   has_many :tests_by, class_name: 'Test', foreign_key: :author_id, dependent: :nullify
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
-  validates :username, presence: true
-  validates :email, presence: true
+  has_secure_password
+
+  validates :email, uniqueness: { case_sensitive: false, message: '%{value} is already taken' }, on: :create
+  validates_format_of :email, with: EMAIL_FORMAT, on: :create
 
   scope :tests_by_level, ->(level) { where(level: level) }
 
