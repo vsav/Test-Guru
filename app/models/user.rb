6d5c6b8c09f74b30b330a8cc55261180d7b90e85
Vadim Savostin <vsavdev@gmail.com>
2019-09-19 07:19:21 +0300

    add devise to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,20 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   EMAIL_FORMAT = /\A[^@\s]+@[^@\s]+\z/.freeze
-
-  #include Auth
 
   has_many :tests_by, class_name: 'Test', foreign_key: :author_id, dependent: :nullify
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
-  has_secure_password
-
   validates :email, uniqueness: { case_sensitive: false, message: '%{value} is already taken' }, on: :create
   validates_format_of :email, with: EMAIL_FORMAT, on: :create
 
   scope :tests_by_level, ->(level) { where(level: level) }
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+         :validatable, :confirmable
 
   def show_tests(level)
     tests.tests_by_level(level)
@@ -23,5 +22,9 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
+  end
+
+  def admin?
+    type == 'Admin'
   end
 end
