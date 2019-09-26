89deb059103c49518f870d8b923c1a93e9741a2b
Vadim Savostin <vsavdev@gmail.com>
2019-09-26 09:25:26 +0300

    add gist creation

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,6 +1,6 @@
 class TestPassagesController < ApplicationController
 
-  before_action :set_test_passage, only: %i[show update result]
+  before_action :set_test_passage, only: %i[show update result gist]
 
   def show; end
 
@@ -14,6 +14,19 @@ class TestPassagesController < ApplicationController
     else
       render 'show'
     end
+  end
+
+  def gist
+    result = GistQuestionService.new(@test_passage.current_question).call
+    @gist = Gist.new(question: @test_passage.current_question,
+                     user: @test_passage.user, gist_url: result[:html_url])
+
+    if @gist.save!
+      flash[:notice] = t('.success', url: (helpers.link_to t('.view_gist'), @gist.gist_url, target: '_blank'))
+    else
+      flash[:alert] = t('.failure')
+    end
+    redirect_to @test_passage
   end
 
   private
