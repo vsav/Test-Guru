b464134e9c4b6d361fd7be55ebd4782c446793d9
Vadim Savostin <vsavdev@gmail.com>
2019-10-01 21:26:01 +0300

    add postgresql, indexes to tests table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_042441) do
+ActiveRecord::Schema.define(version: 2019_10_01_175654) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -66,6 +69,7 @@ ActiveRecord::Schema.define(version: 2019_09_27_042441) do
     t.integer "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,11 @@
 categories = Category.create!([{ title: 'Programming' }, { title: 'Auto' },
                                { title: 'Music' }, { title: 'Movies' }])
 
-users = User.create!([{ username: 'Luke', email: 'luck@rebel.mail' },
-                      { username: 'Han', email: 'han@smuggler.mail' },
-                      { username: 'Lea', email: 'lea@rebel.mail' },
-                      { username: 'Lando', email: 'lando@cloud.city' }])
+users = User.create!([{ username: 'Luke', email: 'luke@rebel.mail', password: 'qweqwe', type: 'Admin',
+                      first_name: 'Luke', last_name: 'Skywalker' },
+                      { username: 'Han', email: 'han@smuggler.mail', password: 'qweqwe' },
+                      { username: 'Lea', email: 'lea@rebel.mail', password: 'qweqwe' },
+                      { username: 'Lando', email: 'lando@cloud.city', password: 'qweqwe' }])
 
 tests = Test.create!([{ title: 'HTML', category: categories[0], level: 0, author: users[0] },
                       { title: 'Ruby', category: categories[0], level: 1, author: users[0] },
