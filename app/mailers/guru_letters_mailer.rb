ab690f1b3cabef87afcc71635a0ebea180731ed2
Vadim Savostin <vsavdev@gmail.com>
2019-10-02 21:08:34 +0300

    mailer fix

--- app/mailers/guru_letters_mailer.rb
+++ app/mailers/guru_letters_mailer.rb
@@ -1,12 +1,12 @@
 class GuruLettersMailer < ApplicationMailer
-  default to: -> { Admin.pluck(:email) },
-          from: @guru_letter.email
+  default from: 'vsavdev@gmail.com'
 
   def contact_us(guru_letter)
     @name = guru_letter.name
     @email = guru_letter.email
     @message = guru_letter.message
+    @admin = User.find_by_type('Admin')
 
-    #mail to: @admin.email, from: guru_letter.email
+    mail to: @admin.email
   end
 end
