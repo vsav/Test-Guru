74e2d24588365c6ba32b2bf7b876031a32e67a63
Vadim Savostin <vsavdev@gmail.com>
2019-10-02 15:49:07 +0300

    create guru_letters controller

---
+++ app/controllers/guru_letters_controller.rb
@@ -0,0 +1,22 @@
+class GuruLettersController < ApplicationController
+  def new
+    @guru_letter = GuruLetter.new
+  end
+
+  def create
+    @guru_letter = GuruLetter.new(letter_params)
+
+    if @guru_letter.valid?
+      GuruLettersMailer.contact_us(@guru_letter).deliver_now
+      redirect_to new_guru_letter_url, notice: t('.success')
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def letter_params
+    params.require(:guru_letter).permit(:name, :email, :message)
+  end
+end
