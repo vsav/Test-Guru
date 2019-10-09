2ac7b3275c478e4838e91fc242c21da4b2dc7e9f
Vadim Savostin <vsavdev@gmail.com>
2019-10-09 19:53:52 +0300

    add condition_value to badges

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_160430) do
+ActiveRecord::Schema.define(version: 2019_10_09_163720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_10_04_160430) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description", null: false
+    t.string "condition_value"
   end
 
   create_table "categories", force: :cascade do |t|
