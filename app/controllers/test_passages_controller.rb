b09ac49220e8e5b973f285ab0656541c5c316d34
Vadim Savostin <vsavdev@gmail.com>
2019-10-11 19:05:41 +0300

    abort test if timer ends

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -8,6 +8,7 @@ class TestPassagesController < ApplicationController
 
   def update
     @test_passage.accept!(params[:answer_ids])
+    @test_passage.abort_test if ( @test_passage.time_left && @test_passage.time_left.zero? )
     if @test_passage.completed?
       award_user(@test_passage)
       TestsMailer.completed_test(@test_passage).deliver_now
@@ -29,8 +30,6 @@ class TestPassagesController < ApplicationController
     end
     redirect_to @test_passage
   end
-
-
 
   private
 
